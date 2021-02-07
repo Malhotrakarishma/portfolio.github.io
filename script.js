fcd2f2f4c95e33eabca903b92154c5519b1f424c
Malhotrakarishma <48879434+Malhotrakarishma@users.noreply.github.com>
2021-02-07 15:24:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  var $homeTop = $(".menu").offset().top;
+  $("#home").click(function () {
+    $("html, body").animate({ scrollTop: $homeTop }, 0);
+  });
+
+  var $aboutTop = $("#about-content").offset().top;
+  $("#about").click(function () {
+    $("html, body").animate({ scrollTop: $aboutTop }, 1000);
+  });
+
+  var $skillTop = $("#skill_id").offset().top;
+  $("#skills").click(function () {
+    $("html, body").animate({ scrollTop: $skillTop }, 1000);
+  });
+
+  $("#contactanchor").click(function () {
+    $(".popup").show();
+  });
+  $(".close").click(function () {
+    $(".popup").hide();
+  });
+});
